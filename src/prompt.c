879e401fe60134d9044f8119d9f2d82a6ad3e252
Juvenn Woo <machese@gmail.com>
2015-10-31 14:33:21 +0800

    Cross platform prompt

--- src/prompt.c
+++ src/prompt.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h> // free
 
-static char input[2048];
+#ifdef _WIN32
+// on Win where ther's no readline
+#include <string.h>
+
+char* readline(char* prompt) {
+  char input[2048];
+  fputs(prompt, stdout);
+  fgets(input, 2048, stdin);
+  char* buffer = malloc(strlen(input) + 1);
+  strcpy(buffer, input);
+  buffer[strlen(input)-1] = '\0';
+  return buffer;
+}
+
+void add_history(char* input) {}
+
+#else
+// on *nix
+#include <editline/readline.h>
+
+#if defined __unix__
+  // only necessary on unix
+  #include <editline/history.h>
+#endif
+
+#endif
 
 int main(int argc, char** argv) {
   puts("Lispy version 0.0.0.0.1");
   puts("Press Ctrl-c to exit.\n");
 
   while (1) {
-    fputs("lispy> ", stdout);
-    fgets(input, 2048, stdin);
-    printf("No you're a %s", input);
+    char* input = readline("lispy> ");
+    add_history(input);
+    printf("No you're a %s\n", input);
+    free(input);
   }
   return 0;
 }
